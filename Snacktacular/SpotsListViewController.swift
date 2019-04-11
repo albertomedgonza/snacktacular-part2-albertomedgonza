01490a689d183f6e8143215a113737f851f47dd6
Alberto Medina <albertomedina@macbook-pro-9.local>
2019-04-11 15:23:26 -0400

    9.3 Complete

--- Snacktacular/SpotsListViewController.swift
+++ Snacktacular/SpotsListViewController.swift
@@ -8,23 +8,50 @@
 
 import UIKit
 import CoreLocation
+import Firebase
+import FirebaseUI
+import GoogleSignIn
 
 class SpotsListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     var spots: Spots!
+    var authUI: FUIAuth!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        authUI = FUIAuth.defaultAuthUI()
+        
+        authUI.delegate = self
+        
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.isHidden = true
         
         spots = Spots()
         spots.spotArray.append(Spot(name: "El Pelon", address: "Comm Ave", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserId: "", documentId: ""))
         spots.spotArray.append(Spot(name: "Chipotle", address: "Cleveland Circle", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserId: "", documentId: ""))
         spots.spotArray.append(Spot(name: "Shake Shack", address: "Newbury", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserId: "", documentId: ""))
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        signIn()
+    }
+    func signIn() {
+        let providers: [FUIAuthProvider] = [
+            FUIGoogleAuth(),
+            ]
+        if authUI.auth?.currentUser == nil {
+           self.authUI.providers = providers
+            present(authUI.authViewController(), animated: true, completion: nil)
+        } else {
+            tableView.isHidden = false
+        }
+        
+    }
+        
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowSpot" {
             let destination = segue.destination as! SpotDetailViewController
@@ -36,6 +63,19 @@ class SpotsListViewController: UIViewController {
             }
         }
     }
+    @IBAction func signOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try authUI.signOut()
+            print("^^^ Succesfuly signed out!")
+            tableView.isHidden = true
+            signIn()
+        } catch {
+            tableView.isHidden = true
+            print("^^^ Couldnt sign out!")
+        }
+        
+    }
+  
 }
 extension SpotsListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -50,4 +90,39 @@ extension SpotsListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
+}
+extension SpotsListViewController: FUIAuthDelegate {
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
+    }
+
+func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+    if let user = user {
+        tableView.isHidden = false
+        print("***We signed in with the user \(user.email ?? "unknown email") ")
+    }
+}
+    func authPickerViewController(forAuthUI authUI: FUIAuth) -> FUIAuthPickerViewController {
+    let loginViewController = FUIAuthPickerViewController(authUI: authUI)
+    loginViewController.view.backgroundColor = UIColor.white
+    
+    let marginInsets: CGFloat = 16
+    let imageHeight: CGFloat = 225
+    let imageY = self.view.center.y - imageHeight
+    let logoFrame = CGRect(x: self.view.frame.origin.x + marginInsets, y: imageY, width: self.view.frame.width - (marginInsets*2), height: imageHeight)
+    let logoImageView = UIImageView(frame: logoFrame)
+    logoImageView.image = UIImage(named: "logo")
+    logoImageView.contentMode = .scaleAspectFit
+    loginViewController.view.addSubview(logoImageView)
+    
+    return loginViewController
+    }
+    
+
 }
