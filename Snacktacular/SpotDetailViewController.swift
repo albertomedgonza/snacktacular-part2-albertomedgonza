e742197c3608987b021725cc43a950c481865ea2
Alberto Medina <albertomedina@macbook-pro-9.local>
2019-04-14 14:04:54 -0400

    9.7

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GooglePlaces
+import MapKit
 
 class SpotDetailViewController: UIViewController {
     
@@ -16,24 +17,39 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var averageRatingLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     var spot: Spot!
+    let regionDistance: CLLocationDistance = 750
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       // mapView.delegate = self
+        
         if spot == nil {
             spot = Spot()
         }
-        nameField.text = spot.name
-        addressField.text = spot.address
+        
+        
+        let region = MKCoordinateRegion(spot.coordinate, regionDistance, regionDistance)
+        mapView.setRegion(region, animated: true)
+        updateUserInterface()
     }
     
     func updateUserInterface() {
         nameField.text = spot.name
         addressField.text = spot.address
+        updateMap()
         
     }
+    func updateMap() {
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(spot)
+        mapView.setCenter(spot.coordinate, animated: true)
+    }
+    
+    
     func leaveViewController() {
         let isPresentingInAddMode = presentingViewController is UINavigationController
         if isPresentingInAddMode {
